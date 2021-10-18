251b7716c9c024952feec21ee06b4f0a50cf21e0
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2021-10-18 12:11:42 +0200

    CCHIP8: Add the remaining code comments

--- cchip8.c
+++ cchip8.c
@@ -8,16 +8,20 @@ int main(int argc, char **argv)
 {
 	printf("CCHIP8 - A C-21 CHIP8 Interpreter Emulator for Linux by cakehonolulu\n");
 	
+	// Check for commandline arguments
 	if (argc < 2)
 	{
 		printf("Usage: ./cchip8 [progname]\n");
 		return FAIL;
 	}
 
+	// Use the FILE directive to access a file
 	FILE *m_prg;
 
+	// Open the file in binary mode (And read-only)
 	m_prg = fopen(argv[1], "rb");
 
+	// Check if the file has been opened
 	if(m_prg == NULL)
 	{
 		printf("Could not open the program file, exiting...\n");
