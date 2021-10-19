090aa5224621af96690444ca329aeea728e3700f
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2021-10-19 10:29:57 +0200

    CCHIP8: Add DXYN instr. sprite draw placeholder switch case and 2NNN (Call to subroutine at NNN)

--- cchip8.c
+++ cchip8.c
@@ -198,6 +198,18 @@ void m_exec(m_chip8 *chip8)
 
     switch(m_opcode & 0xF000)
     {
+    	case 0x2000: // [2NNN] Cals subroutine at NNN
+#ifdef DEBUG
+    		printf("2NNN (%x) [NNN -> 0x%x]\n", chip8->m_currentopcode, chip8->m_currentopcode & 0x0FFF);
+    		printf("2NNN -> PC: 0x%x\n", chip8->m_programcounter);
+#endif
+    		chip8->m_programcounter = chip8->m_currentopcode & 0x0FFF;
+
+#ifdef DEBUG
+    		printf("2NNN Jumped to: 0x%x\n", chip8->m_programcounter);
+#endif
+    		break;
+
     	case 0x6000: // [6XNN] Sets Vx to NN
 #ifdef DEBUG
 			printf("6XNN (%x) [NN -> 0x%x]\n", chip8->m_currentopcode, chip8->m_currentopcode & 0x00FF);
@@ -213,6 +225,14 @@ void m_exec(m_chip8 *chip8)
     			chip8->m_currentopcode & 0x0FFF, chip8->m_currentopcode & 0x0FFF);
 #endif
     		chip8->m_index = chip8->m_currentopcode & 0x0FFF;
+    		chip8->m_programcounter += 2;
+    		break;
+
+    		case 0xD000:
+#ifdef DEBUG
+    		printf("Sprite draw placeholder\n");
+#endif
+
     		chip8->m_programcounter += 2;
     		break;
 
