3143f8603036289717942aa122515014c0191e9b
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2021-10-26 14:03:47 +0200

    CCHIP8: Add a little macro to get NNN addr. from opcode

--- cchip8.h
+++ cchip8.h
@@ -30,6 +30,7 @@
 #define CHIP8_INITIAL_PC 0x200
 
 #define M_OPC_0X00(x)  ((x & 0x0F00) >> 8)
+#define M_GET_NNN_FROM_OPCODE(x) ((x & 0x0FFF))
 
 // Default CHIP8 font
 static const uint8_t m_font[CHIP8_FONT_SIZE] = {
